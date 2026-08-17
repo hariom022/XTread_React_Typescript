99ae38161d625df41875c6c3863ce89e71397fad
Nirnay Biswas <nirnaybis20@gmail.com>
2026-08-17 13:43:21 +0530

    shows casing stage name instead of id in machine page

--- src/master-modules/machines/components/MachineTable.tsx
+++ src/master-modules/machines/components/MachineTable.tsx
@@ -1,24 +1,21 @@
 import type { Machine } from "../types/machine.types";
+import { getCasingStageName } from "../constants/casingStages";
 
 interface MachineTableProps {
   machines: Machine[];
 }
 
-const MachineTable = ({
-  machines,
-}: MachineTableProps) => {
+const MachineTable = ({ machines }: MachineTableProps) => {
   return (
     <div className="machine-table-wrapper">
       <table className="table machine-table mb-0">
         <thead>
           <tr>
-            <th>
-              ID
-            </th>
+            <th>ID</th>
 
             <th>Machine Name</th>
 
-            <th>Casing Stage ID</th>
+            <th>Casing Stage</th>
 
             <th>Sort Order</th>
           </tr>
@@ -31,45 +28,34 @@ const MachineTable = ({
 
           {machines.length === 0 ? (
             <tr>
-              <td
-                colSpan={4}
-                className="machine-empty-state"
-              >
+              <td colSpan={4} className="machine-empty-state">
                 No Records Found
               </td>
             </tr>
           ) : (
             machines.map((item) => (
               <tr key={item.machineId}>
-
                 {/* ID */}
                 <td>
-                  <span className="machine-id">
-                    {item.machineId}
-                  </span>
+                  <span className="machine-id">{item.machineId}</span>
                 </td>
 
                 {/* MACHINE NAME */}
                 <td>
-                  <span className="machine-name">
-                    {item.machineName}
-                  </span>
+                  <span className="machine-name">{item.machineName}</span>
                 </td>
 
                 {/* CASING STAGE */}
                 <td>
-                  <span className="machine-stage-badge">
-                    {item.casingStageId}
+                  <span className="rejection-stage-badge">
+                    {getCasingStageName(item.casingStageId)}
                   </span>
                 </td>
 
                 {/* SORT ORDER */}
                 <td>
-                  <span className="machine-sort-order">
-                    {item.sortOrder}
-                  </span>
+                  <span className="machine-sort-order">{item.sortOrder}</span>
                 </td>
-
               </tr>
             ))
           )}
